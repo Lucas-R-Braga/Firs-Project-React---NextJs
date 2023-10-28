46c7fbe7461f351ff91ab7de18d7e5d646ab5539
Lucas-Braga04 <146142175+Lucas-R-Braga@users.noreply.github.com>
2023-10-28 15:32:10 -0300

    att

--- my-first-app/src/app/App.tsx
+++ my-first-app/src/app/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import {Home} from '../pages/Home'
-import {Contact} from '../pages/Contact';
+import { Home } from '../pages/Home'
+import { Contact } from '../pages/Contact';
 
 export default function App() {
     return (
         <>
-            
-           <header>     
+
+            <header>
                 <h1>Ola</h1>
-                
+
                 <nav>
 
                     <Link to="/Home">Home</Link>
-                                
+
                     <Link to="/Contact">Contato</Link>
 
                 </nav>
-            </header>         
-                
-                <hr />
-                
+            </header>
+
+            <hr />
+
             <Routes>
 
                 <Route path="/" element={<Home />} />
--- my-first-app/src/app/page.tsx
+++ my-first-app/src/app/page.tsx
@@ -4,10 +4,11 @@ import { Home } from '../pages/Home'
 import { Contact } from '../pages/Contact';
 import App from './App';
 
-export default function projeto(){
-  return(
+export default function Home() {
+  return (
     <>
-    
+      
+
     </>
   )
 }
